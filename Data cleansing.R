68e4a31c56c7e403c76f749cebf644533c59812b
CarlosRodriguezSevilla <c.rdz.92@gmail.com>
2016-01-02 16:22:55 +0100

    Data now includes 2010-2015 games

--- Data cleansing.R
+++ Data cleansing.R
@@ -5,50 +5,58 @@ library(tidyr)
 # Load config file with root path, etc
 source("Config.R")
 
-# filenames <- list.files("temp", pattern="*.csv", full.names=TRUE)
-# ldf <- lapply(filenames, read.csv)
+filenames <- list.files(paste0(path, "/tennis_atp-master"), 
+                        pattern="atp_matches_201", 
+                        full.names=TRUE
+                        )
 
-
-# ATP tour-level main draw matches 2015
-matches_2015 <- read.csv(paste0(path, "/tennis_atp-master/atp_matches_2015.csv"))
-
-# Remove matches with non-ranked players
-matches_2015 <- matches_2015[-which(is.na(matches_2015$winner_rank)),]
-matches_2015 <- matches_2015[-which(is.na(matches_2015$loser_rank)),]
-
-# The winner was the one with the highest ranking?
-matches_2015$w_is_better_ranked <- matches_2015$winner_rank < matches_2015$loser_rank
-matches_2015$w_is_better_ranked <- as.factor(matches_2015$w_is_better_ranked)
-
-matches_2015$tourney_date <- as.Date(as.character(matches_2015$tourney_date),format="%Y%m%d")
-
-
+for (i in 1:length(filenames))
+{
+  dataset <- read.csv(filenames[i])
+  
+  # Remove matches with non-ranked players
+  dataset <- dataset[-which(is.na(dataset$winner_rank)),]
+  dataset <- dataset[-which(is.na(dataset$loser_rank)),]
+  
+  # The winner was the one with the highest ranking?
+  dataset$w_is_better_ranked <- dataset$winner_rank < dataset$loser_rank
+  dataset$w_is_better_ranked <- as.factor(dataset$w_is_better_ranked)
+  
+  dataset$tourney_date <- as.Date(as.character(dataset$tourney_date),format="%Y%m%d")
+  
+  if(!exists("matches"))
+  {
+    matches <- dataset
+  }
+  else
+  {
+    matches <- rbind(matches, dataset)
+  }
+    
+}
+rm(i, filenames)
 
 
 
 set.seed(47)
-randomIndex <- sample(x = nrow(matches_2015)
-                      , size = nrow(matches_2015)/2
+randomIndex <- sample(x = nrow(matches)
+                      , size = nrow(matches)/2
 )
 
 for(i in randomIndex){
   
-  winner_cols <- matches_2015[i,8:17]
-  loser_cols  <- matches_2015[i,18:27]
+  winner_cols <- matches[i,8:17]
+  loser_cols  <- matches[i,18:27]
   
-  matches_2015[i,8:17]  <- loser_cols
-  matches_2015[i,18:27] <- winner_cols
+  matches[i,8:17]  <- loser_cols
+  matches[i,18:27] <- winner_cols
   
 }
 rm(i, winner_cols, loser_cols, randomIndex)
 
-colnames(matches_2015) <- gsub("winner", "1st_player", colnames(matches_2015))
-colnames(matches_2015) <- gsub("loser", "2nd_player", colnames(matches_2015))
+colnames(matches) <- gsub("winner", "1st_player", colnames(matches))
+colnames(matches) <- gsub("loser", "2nd_player", colnames(matches))
 
 
 
-
-matches <- matches_2015
-rm(matches_2015)
-
-save.image(paste0(path, "/Matches-clean.RData"))
+save(matches, file=paste0(path, "/Matches-clean.RData"))
