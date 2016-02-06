ca00a86e9d39dc87132f3fc86383a14f9e677556
CarlosRodriguezSevilla <c.rdz.92@gmail.com>
2016-02-06 14:18:27 +0100

    Refactor. Moving code to auxiliar functions file

--- Auxiliar functions.R
+++ Auxiliar functions.R
@@ -1,10 +1,23 @@
 
 
-plotModel <- function(name, performance, table){
+plotModel <- function(name, prediction, model, ytest){
+  
+  # Classification
+  ypred <- predict(object = model, test)
+  resultsDF <- data.frame(ypred, ytest)
+  performance <- performance(prediction, "tpr", "fpr")
+  
+  P  <- nrow( subset ( resultsDF, ypred == 1 ) )
+  N  <- nrow( subset ( resultsDF, ypred == 0 ) )
+  TP <- nrow( subset ( resultsDF, ypred == 1 & ytest == 1 ) )
+  TN <- nrow( subset ( resultsDF, ypred == 0 & ytest == 0 ) )
+  
+  tableModel <- table(PREDICTION = ypred, TRUTH = ytest$w_is_better_ranked)
+  
   png(filename = paste0("output/", name, ".png"), width = 768, height = 1024)
   par(mfrow=c(2, 1), mai=c(0.8,2,2,2)) # mai = c(bottom, left, top, right) 
   plot(performance, main = name, cex.main = 2.5, cex.lab=1.5)
   abline(0,1, lty="dotted")
-  textplot(table, halign = "center", cex = 1.5)
+  textplot(tableModel, halign = "center", cex = 1.5)
   dev.off()
 }
--- Training and testing.R
+++ Training and testing.R
@@ -1,5 +1,5 @@
 
-rm(list=ls()) # Clear workspace
+rm(list=ls(all = TRUE)) # Clear workspace
 
 library(dplyr)
 library(tidyr)
@@ -10,9 +10,8 @@ library(ada)
 library(ROCR)
 library(gplots)
 
-
-# Load config file with root path, etc
-source("Config.R")
+source("Config.R") # Load config file with root path, etc
+source("Auxiliar functions.R")
 
 load(paste0(path, "/Matches-clean.RData"))
 
@@ -30,7 +29,7 @@ rm(trainIndex)
 
 
 # Modeling
-ksvmModel <- ksvm(
+svmModel <- ksvm(
   w_is_better_ranked~.,
   data=train,
   type="C-svc",
@@ -48,59 +47,32 @@ adaModel <- ada(
 
 
 
-# Predict
-ypredProbSVM <- predict(object = ksvmModel
-                 , test
-                 , type="probabilities"
-                 )
-head(ypredProbSVM)
-
-ypredProbADA <- predict(object = adaModel
-                        , test
-                        , type="prob"
-                )
-head(ypredProbADA)
+# Predictor columns
+ypredProbSVM <- predict(object = svmModel, test, type="probabilities")
+ypredProbADA <- predict(object = adaModel, test, type="prob")
 
 
-# ROC Curve
+# Prediction objects
 predSVM <- prediction(ypredProbSVM[,2], na.omit(test)["w_is_better_ranked"])
-perfSVM <- performance(predSVM, "tpr", "fpr")
-
 predADA <- prediction(ypredProbADA[,2], test["w_is_better_ranked"])
-perfADA <- performance(predADA, "tpr", "fpr")
 
-# Clasification
-ypredSVM <- predict(object = ksvmModel, test)
 
-ypredADA <- predict(object = adaModel, test)
 
 # Results
-ytest <- na.omit(test)["w_is_better_ranked"]
-resultsDF <- data.frame(ypredSVM, ytest)
-
-P  <- nrow( subset ( resultsDF, ypredSVM == 1 ) )
-N  <- nrow( subset ( resultsDF, ypredSVM == 0 ) )
-TP <- nrow( subset ( resultsDF, ypredSVM == 1 & ytest == 1 ) )
-TN <- nrow( subset ( resultsDF, ypredSVM == 0 & ytest == 0 ) )
-
-SVMTable <- table(PREDICTION = ypredSVM, TRUTH = ytest$w_is_better_ranked)
-
-
-ytest <- test["w_is_better_ranked"]
-resultsDF <- data.frame(ypredADA, ytest)
-
-P  <- nrow( subset ( resultsDF, ypredADA == 1 ) )
-N  <- nrow( subset ( resultsDF, ypredADA == 0 ) )
-TP <- nrow( subset ( resultsDF, ypredADA == 1 & ytest == 1 ) )
-TN <- nrow( subset ( resultsDF, ypredADA == 0 & ytest == 0 ) )
-
-ADATable <- table(PREDICTION = ypredADA, TRUTH = ytest$w_is_better_ranked)
-
 
 # Create the "/output" folder if it does not already exists
 dir.create('output', showWarnings = F)
 
-# Plot
-plotModel(name="SVM", performance=perfSVM, table = SVMTable)
-plotModel(name="ADA", performance=perfADA, table = ADATable)
+plotModel(
+  name = "SVM", 
+  prediction = predSVM,
+  model = svmModel, 
+  ytest = na.omit(test)["w_is_better_ranked"]
+  )
 
+plotModel(
+  name = "ADA", 
+  prediction = predADA,
+  model = adaModel, 
+  ytest = test["w_is_better_ranked"]
+)
