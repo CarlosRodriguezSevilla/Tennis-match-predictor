8f57e89386b741a2b25915b5ad8e0254dfff481d
carlos <c.rdz.92@gmail.com>
2016-10-24 22:32:41 +0200

    Time stamped script for complete execution.

--- run/RunAll.sh
+++ run/RunAll.sh
@@ -9,45 +9,48 @@ args="--args path='/home/kako/Dev/Data_challenges/Tennis/'"
 
 echo "Running Tennis Match Predictor"
 
+echo -e "[$(date +%H:%M)]" "\t* Extraction and Transformation"
 
 # R CMD BATCH ../src/ExtractionAndTransformation-R.R ../out/R/ExtractionAndTransformation-R.Rout
 ( 
   if R CMD BATCH "${args}" ../src/ExtractionAndTransformation-R.R ../out/R/ExtractionAndTransformation-R.Rout ; then
-    echo -e "\tR done"
+    echo -e "[$(date +%H:%M)]" "\t\t- R done"
   else
-    echo -e "\tR FAILED"
+    echo -e "[$(date +%H:%M)]" "\t\t- R FAILED"
   fi 
 ) & 
 
 # R CMD BATCH ../src/ExtractionAndTransformation-PostgreSQL.R ../out/R/ExtractionAndTransformation-PostgreSQL.Rout
 ( 
   if R CMD BATCH "${args}" ../src/ExtractionAndTransformation-PostgreSQL.R ../out/PostgreSQL/ExtractionAndTransformation-PostgreSQL.Rout ; then 
-    echo -e "\tPostgreSQL done"
+    echo -e "[$(date +%H:%M)]" "\t\t- PostgreSQL done"
   else
-    echo -e "\tPostgreSQL FAILED"
+    echo -e "[$(date +%H:%M)]" "\t\t- PostgreSQL FAILED"
   fi 
 ) & 
 
 # R CMD BATCH ../src/ExtractionAndTransformation-MongoDB.R ../out/R/ExtractionAndTransformation-MongoDB.Rout
 ( 
   if R CMD BATCH "${args}" ../src/ExtractionAndTransformation-MongoDB.R ../out/MongoDB/ExtractionAndTransformation-MongoDB.Rout ; then 
-    echo -e "\tMongoDB done"
+    echo -e "[$(date +%H:%M)]" "\t\t- MongoDB done"
   else
-    echo -e "\tMongoDB FAILED"
+    echo -e "[$(date +%H:%M)]" "\t\t- MongoDB FAILED"
   fi 
 ) &
 
 wait
-echo -e "\t--------------"
 
 
+echo -e "[$(date +%H:%M)]" "\t* Load, Training and Testing"
 # R CMD BATCH ../src/LoadTrainingAndTesting.R ../out/LoadTrainingAndTesting.Rout
 ( 
   if R CMD BATCH "${args}" ../src/LoadTrainingAndTesting.R ../out/LoadTrainingAndTesting.Rout ; then 
-    echo -e "\tLoad, Training and Testing done"
+    echo -e "[$(date +%H:%M)]" "\t\t- Load, Training and Testing done"
   else
-    echo -e "\tLoad, Training and Testing FAILED"
+    echo -e "[$(date +%H:%M)]" "\t\t- Load, Training and Testing FAILED"
   fi 
 )
+
+
 
 echo "DONE"
