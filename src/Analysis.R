f787ab655ec8288623161466f6fea37455ef816c
carlos <c.rdz.92@gmail.com>
2016-11-23 01:11:07 +0100

    Analysis plots.

--- src/Analysis.R
+++ src/Analysis.R
@@ -32,33 +32,53 @@ LoadTrainingAndTesting_csv <- read.table(
   file = "out/LoadTrainingAndTesting/LoadTrainingAndTesting.csv",
   header = T, 
   sep = ",")
+LoadTrainingAndTesting_csv$data_source <- factor(
+  x = LoadTrainingAndTesting_csv$data_source, 
+  levels = sort(unique(LoadTrainingAndTesting_csv$data_source), dec=T))
 
+hues <- seq(15, 375, length = 4)
+cols <- hcl(h = hues, l = 65, c = 100)[1:3]
 
-plot(
-  formula = LoadTrainingAndTesting_csv$end_time~LoadTrainingAndTesting_csv$data_source,
-  xlab=NULL, 
-  ylab = "seconds",
-  las=2)
+default_mar <- par("mar")
 
+png(filename = "out/LTT.png", width=800)
+ggplot(LoadTrainingAndTesting_csv, aes(x=data_source, y=end_time, fill=data_source)) + 
+  geom_boxplot() +
+  scale_x_discrete(name ="Data source") + 
+  scale_y_discrete(name ="Seconds") + 
+  theme(legend.title = element_text(size = 20),
+        legend.text  = element_text(size = 25))
+dev.off()
 
-
+png(filename = "out/r_csv.png")
+par(mar=default_mar + c(4,0,0,0))
 barplot(height = sapply(X = r_csv, FUN = mean), 
-        las    = 2, 
-        ylab = "seconds", 
-        col = "light blue", 
-        main = "R")
+        main = "R",
+        ylab = "seconds",
+        ylim = c(0,30),
+        col = cols[1],
+        las    = 2)
+dev.off()
 
+png(filename = "out/mongodb_csv.png")
+par(mar=default_mar + c(4,0,0,0))
 barplot(height = sapply(X = mongodb_csv, FUN = mean), 
-        las    = 2, 
-        ylab = "seconds", 
-        col = "light blue", 
-        main = "MongoDB")
+        main = "MongoDB",
+        ylab = "seconds",
+        ylim = c(0,30),
+        col = cols[3],
+        las    = 2)
+dev.off()
 
+png(filename = "out/postgresql_csv.png")
+par(mar=default_mar + c(4,0,0,0))
 barplot(height = sapply(X = postgresql_csv, FUN = mean), 
-        las    = 2, 
-        ylab = "seconds", 
-        col = "light blue", 
-        main = "PostgreSQL")
+        main = "PostgreSQL",
+        ylab = "seconds",
+        ylim = c(0,30),
+        col = cols[2],
+        las    = 2)
+dev.off()
 
 mean_data <- data.frame(R=sapply(X = r_csv,          FUN = mean))
 mean_data$MongoDB    <-   sapply(X = mongodb_csv,    FUN = mean)
@@ -67,10 +87,15 @@ mean_data$Mark_time  <- rownames(mean_data)
 rownames(mean_data)  <- NULL
 
 mean_data=reshape2::melt(mean_data, id.vars="Mark_time", variable.name="data_source")
-mean_data$value     <- round( x = mean_data$value, 2)
-mean_data$Mark_time <- factor(x = mean_data$Mark_time, levels = unique(mean_data$Mark_time))
+mean_data$value       <- round( x = mean_data$value, 2)
+mean_data$Mark_time   <- factor(x = mean_data$Mark_time, levels = unique(mean_data$Mark_time))
+mean_data$data_source <- factor(
+  x = mean_data$data_source, 
+  levels = sort(unique(as.character(mean_data$data_source)), dec=T))
 mean_data
 
+png(filename = "out/mean_data.png")
 ggplot(mean_data, aes(x = Mark_time, y = value, fill = data_source)) + 
   geom_bar(stat="identity",position="dodge")+
   xlab(NULL)+ylab("Seconds")
+dev.off()
