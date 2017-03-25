499996bc3e6d92c2fe69a8dc91cc2ff42fac0d2c
carlos <c.rdz.92@gmail.com>
2017-03-26 00:25:40 +0100

    Divide ltt analysis into: 1-load, 2-training and testing.

--- src/Analysis.R
+++ src/Analysis.R
@@ -41,25 +41,47 @@ cols <- hcl(h = hues, l = 65, c = 100)[1:3]
 default_mar <- par("mar")
 
 
-png(filename = "out/mean_data_ltt.png", width=800)
-mean_data_ltt=reshape2::melt(LoadTrainingAndTesting_csv, id.vars="data_source", variable.name="interval")
-mean_data_ltt[is.na(mean_data_ltt$value), "value"] <- 0
-mean_data_ltt$data_source <- factor(
-  x = mean_data_ltt$data_source, 
-  levels = sort(unique(as.character(mean_data_ltt$data_source)), dec=T))
-ggplot(data = mean_data_ltt, 
+png(filename = "out/mean_data_l.png", width=800)
+mean_data_l=reshape2::melt(data          = LoadTrainingAndTesting_csv, 
+                             id.vars       = "data_source", 
+                             variable.name = "interval", 
+                             measure.vars  = 2:4)
+mean_data_l[is.na(mean_data_l$value), "value"] <- 0
+mean_data_l$data_source <- factor(
+  x = mean_data_l$data_source, 
+  levels = sort(unique(as.character(mean_data_l$data_source)), dec=T))
+ggplot(data = mean_data_l, 
        aes(x = interval, y = value, fill = data_source)) + 
   geom_bar(stat="identity",position="dodge")+
   xlab(NULL)+ylab("Seconds")
 dev.off()
 
+png(filename = "out/mean_data_tt.png", width=800)
+mean_data_tt=reshape2::melt(data          = LoadTrainingAndTesting_csv, 
+                             id.vars       = "data_source", 
+                             variable.name = "interval", 
+                             measure.vars  = 5:length(LoadTrainingAndTesting_csv))
+mean_data_tt[is.na(mean_data_tt$value), "value"] <- 0
+mean_data_tt$data_source <- factor(
+  x = mean_data_tt$data_source, 
+  levels = sort(unique(as.character(mean_data_tt$data_source)), dec=T))
+ggplot(data = mean_data_tt, 
+       aes(x = interval, y = value, fill = data_source)) + 
+  stat_summary(fun.y="mean", position=position_dodge(), geom="bar") + 
+  xlab(NULL)+ylab("Seconds")
+dev.off()
+
+
+ylim_group <- c(0, max(c(apply(X = r_csv,          FUN = mean, MARGIN = 2), 
+                         apply(X = mongodb_csv,    FUN = mean, MARGIN = 2), 
+                         apply(X = postgresql_csv, FUN = mean, MARGIN = 2))))
 
 png(filename = "out/r_csv.png", width = 800)
 par(mar = default_mar + c(4,0,0,0))
 barplot(height = sapply(X = r_csv, FUN = mean), 
         main = "R",
         ylab = "seconds",
-        ylim = c(0,20),
+        ylim = ylim_group,
         col = cols[1],
         las    = 2,
         border = NA)
@@ -71,7 +93,7 @@ par(mar = default_mar + c(4,0,0,0))
 barplot(height = sapply(X = mongodb_csv, FUN = mean), 
         main = "MongoDB",
         ylab = "seconds",
-        ylim = c(0,20),
+        ylim = ylim_group,
         col = cols[3],
         las    = 2,
         border = NA)
@@ -83,7 +105,7 @@ par(mar = default_mar + c(4,0,0,0))
 barplot(height = sapply(X = postgresql_csv, FUN = mean), 
         main = "PostgreSQL",
         ylab = "seconds",
-        ylim = c(0,20),
+        ylim = ylim_group,
         col = cols[2],
         las    = 2,
         border = NA)
